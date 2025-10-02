95470e8e9ff508f47da62d0c4f88843f3fbf128a
Colin Sonnenberg <sonnenco@oregonstate.edu>
2025-10-02 16:49:38 -0400

    .

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,5 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   server: {host: true, port: 5173,},
   plugins: [react(), tailwindcss(),],
-  base: "/colin-portfolio",
+  base: "/colin-portfolio-site",
 })
